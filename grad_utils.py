a822e6ab9a78bfd303194ea70ee995c3a7d29977
Andrew Hess <ahess3815@gmail.com>
2019-02-24 17:04:34 -0800

    Added option to use biases for U and V

--- grad_utils.py
+++ grad_utils.py
@@ -2,46 +2,92 @@ import numpy as np
 import numpy as np
 import matplotlib.pyplot as plt
 
-def grad_U(Ui, Yij, Vj, reg, eta):
+def grad_U(Ui, Yij, Vj, reg, eta, ai=None, bj=None, mu=None):
     """
     Takes as input Ui (the ith row of U), a training point Yij, the column
     vector Vj (jth column of V^T), reg (the regularization parameter lambda),
-    and eta (the learning rate).
+    eta (the learning rate), and optionally ai, bj, mu (the bias of the ith
+    user, jth movie, and the average value in Y). If ai, bj, and mu are not
+    given, they are not used in the loss function.
 
     Returns the gradient of the regularized loss function with
     respect to Ui multiplied by eta.
     """
-    return eta * (reg * Ui - Vj.dot((Yij - Ui.transpose().dot(Vj)).transpose()))
+    if ai is None or bj is None or mu is None:
+        grad = reg * Ui - Vj.dot((Yij - Ui.transpose().dot(Vj)).transpose())
+    else:
+        grad = reg * Ui - Vj.dot((Yij - mu - Ui.transpose().dot(Vj) - ai - bj))
 
-def grad_V(Vj, Yij, Ui, reg, eta):
+    return eta * grad
+
+def grad_V(Vj, Yij, Ui, reg, eta, ai=None, bj=None, mu=None):
     """
     Takes as input the column vector Vj (jth column of V^T), a training point Yij,
     Ui (the ith row of U), reg (the regularization parameter lambda),
-    and eta (the learning rate).
+    eta (the learning rate), and optionally ai, bj, mu (the bias of the ith
+    user, jth movie, and the average value in Y). If ai, bj, and mu are not
+    given, they are not used in the loss function.
 
     Returns the gradient of the regularized loss function with
     respect to Vj multiplied by eta.
     """
-    return eta * (reg * Vj - Ui.dot((Yij - Ui.transpose().dot(Vj)).transpose()))
+    if ai is None or bj is None or mu is None:
+        grad = reg * Vj - Ui.dot((Yij - Ui.transpose().dot(Vj)).transpose())
+    else:
+        grad = reg * Vj - Ui.dot((Yij - mu - Ui.transpose().dot(Vj) - ai - bj))
 
-def get_err(U, V, Y, reg=0.0):
+    return eta * grad
+
+def grad_ai(Ui, Yij, Vj, ai, bj, mu, reg, eta):
+    """
+    Takes as input Ui (the ith row of U), a training point Yij, the column
+    vector Vj (jth column of V^T), ai (the bias for the ith user), bj (the bias
+    for the jth movie), mu (the average value in Y), reg (the regularization
+    parameter lambda), and eta (the learning rate).
+
+    Returns the gradient of the regularized loss function with respect to ai
+    multiplied by eta.
+    """
+    return eta * (reg * ai + (Yij - mu - Ui.transpose().dot(Vj) - ai - bj))
+
+def grad_bj(Vj, Yij, Ui, ai, bj, mu, reg, eta):
+    """
+    Takes as input Vj (the jth column of U), a training point Yij, the row
+    vector Ui (ith row of U), ai (the bias for the ith user), bj (the bias
+    for the jth movie), mu (the average value in Y), reg (the regularization
+    parameter lambda), and eta (the learning rate).
+
+    Returns the gradient of the regularized loss function with respect to bj
+    multiplied by eta.
+    """
+    return eta * (reg * bj + (Yij - mu - Ui.transpose().dot(Vj) - ai - bj))
+
+def get_err(U, V, Y, reg=0.0, a=None, b=None, mu=None):
     """
     Takes as input a matrix Y of triples (i, j, Y_ij) where i is the index of a user,
     j is the index of a movie, and Y_ij is user i's rating of movie j and
-    user/movie matrices U and V.
+    user/movie matrices U and V. Also optionally takes a, b, and mu, the user
+    and movie biases, and the average value in Y. If a, b, or mu is not given,
+    the bias is not used to compute the error.
 
     Returns the mean regularized squared-error of predictions made by
     estimating Y_{ij} as the dot product of the ith row of U and the jth column of V^T.
     """
     r_term = reg * (np.trace(U.transpose().dot(U)) + np.trace(V.transpose().dot(V)))
+
+    if a is not None and b is not None and mu is not None:
+        r_term += reg * (np.sum(np.square(a)) + np.sum(np.square(b)))
+
     l_term = 0
     for i, j, Yij in Y:
-        l_term += (Yij - U[i-1].transpose().dot(V[j-1])) ** 2
+        if a is None or b is None or mu is None:
+            l_term += (Yij - U[i-1].transpose().dot(V[j-1])) ** 2
+        else:
+            l_term += (Yij - mu - U[i-1].transpose().dot(V[j-1]) - a[i-1] - b[j-1]) ** 2
 
     return (r_term + l_term) / 2 / len(Y)
 
-def train_model(M, N, K, eta, reg, Y, eps=0.0001, max_epochs=300):
-    print(Y)
+def train_model(M, N, K, eta, reg, Y, eps=0.0001, max_epochs=300, bias=False):
     """
     Given a training data matrix Y containing rows (i, j, Y_ij)
     where Y_ij is user i's rating on movie j, learns an
@@ -54,28 +100,41 @@ def train_model(M, N, K, eta, reg, Y, eps=0.0001, max_epochs=300):
     MSE after the first epoch.
 
     Returns a tuple (U, V, err) consisting of U, V, and the unregularized MSE
-    of the model.
+    of the model if no bias is used. If bias is used, it returns (U, V, a, b, mu, err).
     """
     U = np.random.rand(M, K) - 0.5
     V = np.random.rand(N, K) - 0.5
 
-    #print(U.shape)
-    #print(V.shape)
+    # Setup the bias parameters.
+    if bias:
+        a = np.random.random_sample(U.shape[0]) - 0.5
+        b = np.random.random_sample(V.shape[0]) - 0.5
+        mu = np.mean(Y[:, 2])
 
     for epoch in range(max_epochs):
-        init_loss = get_err(U, V, Y)
+        init_loss = get_err(U, V, Y, a=a, b=b, mu=mu) if bias else get_err(U, V, Y)
         print(epoch, "|", init_loss)
 
         np.random.shuffle(Y)
 
         for i, j, Yij in Y:
-            U[i-1] -= grad_U(U[i-1], Yij, V[j-1], reg, eta)
-            V[j-1] -= grad_V(V[j-1], Yij, U[i-1], reg, eta)
+            if bias:
+                U[i-1] -= grad_U(U[i-1], Yij, V[j-1], reg, eta, a[i-1], b[j-1], mu)
+                V[j-1] -= grad_V(V[j-1], Yij, U[i-1], reg, eta, a[i-1], b[j-1], mu)
+                a[i-1] -= grad_ai(U[i-1], Yij, V[j-1], a[i-1], b[j-1], mu, reg, eta)
+                b[j-1] -= grad_bj(V[j-1], Yij, U[i-1], a[i-1], b[j-1], mu, reg, eta)
+            else:
+                U[i-1] -= grad_U(U[i-1], Yij, V[j-1], reg, eta)
+                V[j-1] -= grad_V(V[j-1], Yij, U[i-1], reg, eta)
 
-        if init_loss - get_err(U, V, Y) < eps:
-            return (U, V, get_err(U, V, Y))
 
-    return (U, V, get_err(U, V, Y))
+        err = get_err(U, V, Y, a=a, b=b, mu=mu) if bias else get_err(U, V, Y)
+
+        if init_loss - err < eps:
+            print(epoch, "|", err)
+            break
+
+    return (U, V, a, b, mu, err) if bias else (U, V, err)
 
 def get_projected(V):
     A, S, B = np.linalg.svd(V,full_matrices=False)
--- p5_visualization.py
+++ p5_visualization.py
@@ -1,9 +1,15 @@
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from grad_utils import train_model, get_err, get_projected, draw_projection
 from basic_visualization import get_popular_ids, get_best_ids, get_movies_dict
 
 def main():
+    # Check the command line arguments if for bias should be used.
+    bias = (len(sys.argv) == 2 and sys.argv[1] == '-bias')
+
+    print('using bias:', bias)
+
     Y_train = np.loadtxt('data/train.txt').astype(int)
     Y_test = np.loadtxt('data/test.txt').astype(int)
 
@@ -18,10 +24,14 @@ def main():
     reg = 0.1
     eps = 0.0001
 
-    U,V, err = train_model(M, N, K, eta, reg, Y_train)
-    eout = get_err(U, V, Y_test)
+    # Train the model.
+    if bias:
+        U, V, a, b, mu, err = train_model(M, N, K, eta, reg, Y_train, bias=True)
+    else:
+        U, V, err = train_model(M, N, K, eta, reg, Y_train)
+
+    eout = get_err(U, V, Y_test, a=a, b=b, mu=mu) if bias else get_err(U, V, Y_test)
     print(err, eout)
-    #print(V.shape)
     movies = get_projected(V.transpose()).transpose()
     data = np.loadtxt('data/data.txt')
     movies_dict = get_movies_dict(data)
@@ -42,14 +52,15 @@ def main():
     crime_ids = np.array(get_popular_ids(movies_dict, 'crime')).astype(int)
     war_ids = np.array(get_popular_ids(movies_dict, 'war')).astype(int)
 
+    draw_projection(movies, [action_ids, crime_ids, war_ids])
+
     genre_index = {'unknown': 1, 'action': 2, 'adventure': 3, 'animation': 4,
                    'childrens': 5, 'comedy': 6, 'crime': 7, 'documentary': 8,
                    'drama': 9, 'fantasy': 10, 'film-noir': 11, 'horror': 12,
                    'musical': 13, 'mystery': 14, 'romance': 15, 'sci-fi': 16,
                    'thriller': 17, 'war': 18, 'western': 19}
     for genre in genre_index.keys():
-    draw_projection(movies, [np.array(get_popular_ids(movies_dict, 'war')).astype(int)])
-    draw_projection(movies, [action_ids, crime_ids, war_ids])
+        draw_projection(movies, [np.array(get_popular_ids(movies_dict, 'war')).astype(int)])
 
 if __name__ == "__main__":
     main()
